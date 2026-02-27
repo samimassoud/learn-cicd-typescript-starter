795d931959be1c05ffe5396325892c549e0a76e5
samimassoud <samimassoud67@gmail.com>
2026-02-28 00:36:46 +0200

    Broken test code test

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+    isActive: true,
+    age: 32,
+};
+
+describe("person", () => {
+    test("person is defined", () => {
+        expect(person).toBeDefined();
+    });
+
+    test("is active", () => {
+        expect(person.
+});
